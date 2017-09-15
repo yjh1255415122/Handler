e72337e4b34af1cb9457fe987dd5bd5aed056584
jian hao yang <jian hao yang>
2017-09-15 13:52:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jianhao/handler/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.jianhao.handler;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.tv1);
+        final Handler handler = new Handler() {
+            @Override
+        public void handleMessage(Message msg) {
+        textView.setText(msg.arg1+"");
+    }
+
+};
+        final Runnable myWorker = new Runnable() {
+        @Override
+        public void run() {
+        int progress = 0;
+        while(progress <= 10){
+        Message msg = new Message();
+        msg.arg1 = progress;
+        handler.sendMessage(msg);
+        progress += 1;
+        try {
+        Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        e.printStackTrace();
+        }
+        }
+        Message msg = handler.obtainMessage();//同 new Message();
+                    msg.arg1 = -1;
+        handler.sendMessage(msg);
+        }
+        };
+        Button button = (Button) findViewById(R.id.bt1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread workThread = new Thread(null, myWorker, "WorkThread");
+                workThread.start();
+            }
+        });
+    }
+
+    }
